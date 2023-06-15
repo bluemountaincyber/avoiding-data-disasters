99dfa9600e7c430e6e0afa5dcdc15812cfbe2c90
ryananicholson <ryananicholson@gmail.com>
2023-06-15 13:57:48 -0400

    More changes

--- build.sh
+++ build.sh
@@ -6,10 +6,28 @@ NOCOLOR="\033[0m"
 
 echo -en $GREEN"Deploying CloudFormation stack... "$NOCOLOR
 aws cloudformation create-stack --stack-name avoiding-data-disasters --template-body file://TotallySecure.yaml
-echo "Done"
+while true; do
+    case $(aws cloudformation describe-stacks --query 'Stacks[].StackStatus' --output text) in
+        CREATE_COMPLETE)
+            echo "Done"
+            break
+            ;;
+        CREATE_FAILED)
+            echo -e $RED"FAILED"
+            exit 1
+            ;;
+        CREATE_IN_PROGRESS)
+            sleep 5
+            ;;
+        *)
+            echo -e $RED"FAILED"
+            exit 1
+            ;;
+    esac
+done
 
 echo -en $GREEN"Uploading sensitive data... "$NOCOLOR
 BUCKET=$(aws s3 ls | grep sensitive- | awk '{print $3}')
-pushd ~/avoiding-data-disasters
+pushd ~/avoiding-data-disasters >/dev/null
 aws s3 cp customers.csv s3://$BUCKET/customers.csv
-popd
+popd >/dev/null
