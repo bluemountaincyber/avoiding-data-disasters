56050dbdf0728df2f25622d212a93d2fc047b523
ryananicholson <ryananicholson@gmail.com>
2023-06-16 08:34:43 -0400

    fix: Completed build and destroy scripts

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ RED="\033[31m"
 NOCOLOR="\033[0m"
 
 echo -en $GREEN"Deploying CloudFormation stack... "$NOCOLOR
-aws cloudformation create-stack --stack-name avoiding-data-disasters --template-body file://TotallySecure.yaml
+aws cloudformation create-stack --stack-name avoiding-data-disasters --template-body file://TotallySecure.yaml >/dev/null
 while true; do
     case $(aws cloudformation describe-stacks --query 'Stacks[].StackStatus' --output text) in
         CREATE_COMPLETE)
@@ -20,14 +20,30 @@ while true; do
             sleep 5
             ;;
         *)
-            echo -e $RED"FAILED"
+            echo -e $RED"FAILED"$NOCOLOR
             exit 1
             ;;
     esac
 done
 
-echo -en $GREEN"Uploading sensitive data... "$NOCOLOR
+echo -en $GREEN"Setting bucket ACL... "$NOCOLOR
 BUCKET=$(aws s3 ls | grep sensitive- | awk '{print $3}')
+aws s3api put-bucket-acl --bucket $BUCKET --acl public-read >/dev/null
+if [[ $(aws s3api get-bucket-acl --bucket $BUCKET --query 'Grants[?Permission==`READ`].Grantee.URI' --output text) == "http://acs.amazonaws.com/groups/global/AllUsers" ]]; then
+    echo "Done"
+else
+    echo $RED"FAILED"$NOCOLOR
+    exit 1
+fi
+
+echo -en $GREEN"Uploading sensitive data... "$NOCOLOR
 pushd ~/avoiding-data-disasters >/dev/null
-aws s3 cp customers.csv s3://$BUCKET/customers.csv
+aws s3 cp customers.csv s3://$BUCKET/customers.csv >/dev/null
 popd >/dev/null
+aws s3 ls s3://$BUCKET/customers.csv >/dev/null
+if [ $? -eq 0 ]; then
+    echo "Done"
+else
+    echo -e $RED"FAILED"$NOCOLOR
+    exit 1
+fi
--- destroy.sh
+++ destroy.sh
@@ -5,9 +5,22 @@ RED="\033[31m"
 NOCOLOR="\033[0m"
 
 BUCKET=$(aws s3 ls | grep sensitive | awk '{print $3}')
-echo -en $GREEN"Emptying sensitive-$BUCKET bucket... "$NOCOLOR
-aws s3 rm --recursive s3://$BUCKET
+echo -en $GREEN"Emptying $BUCKET bucket... "$NOCOLOR
+aws s3 rm --recursive s3://$BUCKET >/dev/null
+if ! [[ $(aws s3 ls s3://$BUCKET/customers.csv) ]]; then
+    echo "Done"
+else
+    echo -e $RED"FAILED"$NOCOLOR
+    exit 1
+fi
 
 echo -en $GREEN"Destroying CloudFormation stack... "$NOCOLOR
 aws cloudformation delete-stack --stack-name avoiding-data-disasters
-echo "Done"
+while true; do
+    if [[ $(aws cloudformation describe-stacks --query 'Stacks[?StackName==`avoiding-data-disasters`].StackName' --output text) == "avoiding-data-disasters" ]]; then
+        sleep 5
+    else
+        echo "Done"
+        break
+    fi
+done
