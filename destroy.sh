f5ea6ae82264b6b5a8859ef495461aa6d1c46d6b
ryananicholson <ryananicholson@gmail.com>
2023-07-09 11:32:35 -0400

    fix: delete versions

--- destroy.sh
+++ destroy.sh
@@ -6,7 +6,17 @@ NOCOLOR="\033[0m"
 
 BUCKET=$(aws s3 ls | grep sensitive | awk '{print $3}')
 echo -en $GREEN"Emptying $BUCKET bucket... "$NOCOLOR
-aws s3 rm --recursive s3://$BUCKET >/dev/null
+aws s3api put-bucket-versioning --bucket $BUCKET --versioning-configuration Status=Suspended
+for OBJECT in $(aws s3api list-object-versions --bucket $BUCKET | jq -r '"\(.DeleteMarkers[].Key),\(.DeleteMarkers[].VersionId)"'); do
+  KEY=$(echo $OBJECT | cut -d ',' -f1)
+  VERSIONID=$(echo $OBJECT | cut -d ',' -f2)
+  aws s3api delete-object --bucket $BUCKET --version-id $VERSIONID --key $KEY 2>/dev/null >/dev/null
+done
+for OBJECT in $(aws s3api list-object-versions --bucket $BUCKET | jq -r '"\(.Versions[].Key),\(.Versions[].VersionId)"'); do
+  KEY=$(echo $OBJECT | cut -d ',' -f1)
+  VERSIONID=$(echo $OBJECT | cut -d ',' -f2)
+  aws s3api delete-object --bucket $BUCKET --version-id $VERSIONID --key $KEY 2>/dev/null >/dev/null
+done
 if ! [[ $(aws s3 ls s3://$BUCKET/customers.csv) ]]; then
     echo "Done"
 else
